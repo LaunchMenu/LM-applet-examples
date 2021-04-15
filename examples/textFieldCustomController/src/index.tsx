860c921ecb4a69f20ddef4f5baf7582027ecd9f8
TarVK <tarvankrieken@gmail.com>
2021-04-15 21:48:22 +0200

    Added text field examples

---
+++ examples/textFieldCustomController/src/index.tsx
@@ -0,0 +1,66 @@
+import {
+    createSettings,
+    createSettingsFolder,
+    createStandardTextFieldKeyHandler,
+    declare,
+    IKeyEventListener,
+    insertText,
+    Menu,
+    TextField,
+    UILayer,
+} from "@launchmenu/core";
+
+const info = {
+    name: "Example",
+    description: "A minimal example applet",
+    version: "0.0.0",
+    icon: "applets" as const,
+};
+
+const settings = createSettings({
+    version: "0.0.0",
+    settings: () =>
+        createSettingsFolder({
+            ...info,
+            children: {},
+        }),
+});
+
+export default declare({
+    info,
+    settings,
+    open({context, onClose}) {
+        context.open(
+            new UILayer(
+                (context, close) => {
+                    const field = new TextField();
+                    const baseHandler = createStandardTextFieldKeyHandler(
+                        field,
+                        context,
+                        {onExit: close}
+                    );
+                    const handler: IKeyEventListener = event => {
+                        const index = event.key.char
+                            ? "abcdefghijklmnopqrstuvwxyz".indexOf(event.key.char)
+                            : -1;
+                        if (index != -1 && ["down", "repeat"].includes(event.type)) {
+                            insertText(field, index + "");
+                            return true;
+                        }
+
+                        return baseHandler(event);
+                    };
+
+                    return {
+                        field,
+                        fieldHandler: handler,
+                        onClose,
+                    };
+                },
+                {
+                    path: "Example",
+                }
+            )
+        );
+    },
+});
